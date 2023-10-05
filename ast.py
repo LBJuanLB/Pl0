9c52d41869723c3ab61a799a0e95c6ab33975251
angola031 <c.angola@utp.edu.co>
2023-10-05 14:42:16 -0500

    actualizando ast

--- ast.py
+++ ast.py
@@ -1,27 +1,31 @@
 from dataclasses import dataclass
 from multimethod import multimeta
-class  Visitor(multimeta):
+
+class  Visitor(metaclass=multimeta):
     ...
 @dataclass
 class Node:
-    def accept(self,n:Visitor,*args, **kwargs):
-        ...
+    def accept(self,v:Visitor,*args, **kwargs):
+        return v.visit(self, *args,**kwargs)
 @dataclass
 class Expression(Node):
     ...
 @dataclass
 class Binary(Expression):
-    ...
+    op :str
+    lefth: Expression
+    rigth: Expression
 @dataclass
 class lefth(Binary):
     ...
 @dataclass
 class rigth(Binary):
     ...
-    
 @dataclass
 class Number(Expression):
     value: int
+    
+    
 class Eval(Visitor):
     @classmethod
     def eval(cls,n:Node):
@@ -30,6 +34,8 @@ class Eval(Visitor):
     def visit(self, n:Number):
         return n.value
     def visit(self, n:Binary):
-        left=n.accept(n.lefth)
-        rigth=n.accept(n.rigth)
+        left=n.lefth.accept(n.lefth)
+        rigth=n.rigth.accept(n.rigth)
+        return eval(f"{left}{n.op}{rigth}")
+            
         
